5a6e297a0931cf0bb206c118cb88a4001d22bc3c
YongSangHo <yonghole@ajou.ac.kr>
2021-04-10 19:20:04 +0900

    initial commit

---
+++ src/main/java/com/mola/mola/MolaApplication.java
@@ -0,0 +1,13 @@
+package com.mola.mola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mola/mola/MolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mola.mola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
