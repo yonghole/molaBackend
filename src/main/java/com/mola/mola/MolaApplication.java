ff5840c0001b02e8588621ae92bbc3b25444b694
YongSangHo <yonghole@ajou.ac.kr>
2021-05-24 13:15:42 +0900

    return to jar

--- src/main/java/com/mola/mola/MolaApplication.java
+++ src/main/java/com/mola/mola/MolaApplication.java
@@ -6,16 +6,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MolaApplication extends SpringBootServletInitializer {
-
-
+public class MolaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MolaApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(MolaApplication.class);
-	}
+
 }
