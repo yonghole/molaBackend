169bed9f726593c8283dbcd532694d7acfb31bf2
YongSangHo <yonghole@ajou.ac.kr>
2021-05-24 11:17:56 +0900

    change to war

--- src/main/java/com/mola/mola/MolaApplication.java
+++ src/main/java/com/mola/mola/MolaApplication.java
@@ -2,9 +2,16 @@ package com.mola.mola;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MolaApplication {
+public class MolaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MolaApplication.class, args);
